df45faaf6ddac460805cb4fa206b0d948c99298a
Jenaru0 <2201080232@undc.edu.pe>
2025-06-12 11:55:23 -0500

    fix: estructura backend

--- web/src/app/categorias/helados/page.tsx
+++
@@ -1,9 +0,0 @@
-'use client'
-
-export default function CategoriaHeladoPage() {
-    return(
-        <div>
-            <h1>  Pagina de categoria helados</h1>
-        </div>
-    );
-}
--- web/src/components/carrito/carritoDrawer/DrawerEmpty.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const DrawerEmpty: React.FC = () => (
-  <div className="text-center text-gray-400 py-12 sm:py-20 text-base sm:text-lg font-medium">
-    ¡Tu carrito está vacío!
-  </div>
-);
-
-export default DrawerEmpty;
